912efa34236eee43155305e4f1b5ab952c5a4638
BrandonAGr <BrandonAGr@gmail.com>
2013-08-26 22:18:41 -0500

    Begin adding timeline that generates the time of each step movement, need a better testing approach

---
+++ src/plotter/speedProfile.go
@@ -0,0 +1,5 @@
+package plotter
+
+// Manages the constant or trapezoidal speed of the plot head along a path of coordinages
+
+// todo: write interface, and simple implementation, tests
---
+++ src/plotter/timeline.go
@@ -0,0 +1,96 @@
+package plotter
+
+// Takes a stream of plotCoords and generates
+
+import (
+	"fmt"
+	"math"
+)
+
+// Takes in coordinates and outputs a timeilne of step events
+func GenerateTimeline(plotCoords <-chan Coordinate) {
+
+	polarSystem := PolarSystemFromSettings()
+	previousPolarPos := PolarCoordinate{LeftDist: Settings.StartingLeftDist_MM, RightDist: Settings.StartingRightDist_MM}
+	startingLocation := previousPolarPos.ToCoord(polarSystem)
+	mmToSteps := 1 / Settings.StepSize_MM
+
+	fmt.Println("Start Location", startingLocation, "Initial Polar", previousPolarPos)
+
+	if startingLocation.IsNaN() {
+		panic(fmt.Sprint("Starting location is not a valid number, your settings.xml has impossible values"))
+	}
+
+	// setup 0,0 as the initial location of the plot head
+	polarSystem.XOffset = startingLocation.X
+	polarSystem.YOffset = startingLocation.Y
+
+	target, chanOpen := <-plotCoords
+	if !chanOpen {
+		return
+	}
+	target = target.Add(startingLocation).Scaled(mmToSteps)
+	anotherTarget := true
+
+	for anotherTarget {
+		nextTarget, chanOpen := <-plotCoords
+		nextTarget = nextTarget.Add(startingLocation).Scaled(mmToSteps)
+		if !chanOpen {
+			anotherTarget = false
+			nextTarget = target
+		}
+
+		currentPolarPosSteps := previousPolarPos.Scaled(mmToSteps).Ceil()
+
+		for {
+			lineSegment := LineSegment{target, nextTarget}
+			intersectionTime := math.MaxFloat64
+			intersectionIndex := -1
+
+			for index, circle := range []Circle{
+				Circle{Radius: currentPolarPosSteps.LeftDist - 1},
+				Circle{Radius: currentPolarPosSteps.LeftDist + 1},
+				Circle{Radius: currentPolarPosSteps.RightDist - 1, Center: Coordinate{X: polarSystem.RightMotorDist * mmToSteps, Y: 0}},
+				Circle{Radius: currentPolarPosSteps.RightDist + 1, Center: Coordinate{X: polarSystem.RightMotorDist * mmToSteps, Y: 0}}} {
+
+				fmt.Println("Intersecting ", lineSegment, circle)
+
+				if time, valid := circle.IntersectionTime(lineSegment); valid && time > 0 && time < intersectionTime {
+					intersectionTime = time
+					intersectionIndex = index
+				}
+			}
+
+			if intersectionIndex == -1 {
+				break
+			}
+
+			// need to also update the currentPolarPosSteps according to the current position
+
+			fmt.Println("Intersection at", intersectionTime, target, "for", intersectionIndex)
+			target = target.Add(nextTarget.Minus(target).Scaled(intersectionTime))
+			fmt.Println("Moved to", target)
+
+			fmt.Println("Polar from", currentPolarPosSteps)
+			switch intersectionIndex {
+			case 0:
+				currentPolarPosSteps.LeftDist -= 1
+				break
+			case 1:
+				currentPolarPosSteps.LeftDist += 1
+				break
+			case 2:
+				currentPolarPosSteps.RightDist -= 1
+				break
+			case 3:
+				currentPolarPosSteps.RightDist += 1
+				break
+			}
+			fmt.Println("to", currentPolarPosSteps)
+
+		}
+
+		target = nextTarget
+	}
+	fmt.Println("Done generating timeline")
+}
